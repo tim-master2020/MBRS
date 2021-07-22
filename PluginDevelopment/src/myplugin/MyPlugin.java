09c4bc2461fc2bb340336003bdc96d6991de62d1
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2021-07-22 18:22:12 +0200

    initial commit

---
+++ PluginDevelopment/src/myplugin/MyPlugin.java
@@ -0,0 +1,54 @@
+package myplugin;
+
+import java.io.File;
+
+import javax.swing.JOptionPane;
+
+import myplugin.generator.options.GeneratorOptions;
+import myplugin.generator.options.ProjectOptions;
+
+
+import com.nomagic.actions.NMAction;
+import com.nomagic.magicdraw.actions.ActionsConfiguratorsManager;
+
+/** MagicDraw plugin that performes code generation */
+public class MyPlugin extends com.nomagic.magicdraw.plugins.Plugin {
+	
+	String pluginDir = null; 
+	
+	public void init() {
+		JOptionPane.showMessageDialog( null, "My Plugin init");
+		
+		pluginDir = getDescriptor().getPluginDirectory().getPath();
+		
+		// Creating submenu in the MagicDraw main menu 	
+		ActionsConfiguratorsManager manager = ActionsConfiguratorsManager.getInstance();		
+		manager.addMainMenuConfigurator(new MainMenuConfigurator(getSubmenuActions()));
+		
+		/** @Todo: load project options (@see myplugin.generator.options.ProjectOptions) from 
+		 * ProjectOptions.xml and take ejb generator options */
+		
+		//for test purpose only:
+		GeneratorOptions ejbOptions = new GeneratorOptions("c:/temp", "ejbclass", "templates", "{0}.java", true, "ejb"); 				
+		ProjectOptions.getProjectOptions().getGeneratorOptions().put("EJBGenerator", ejbOptions);
+				
+		ejbOptions.setTemplateDir(pluginDir + File.separator + ejbOptions.getTemplateDir()); //apsolutna putanja
+	}
+
+	private NMAction[] getSubmenuActions()
+	{
+	   return new NMAction[]{
+			new GenerateAction("Generate"),
+	   };
+	}
+	
+	public boolean close() {
+		return true;
+	}
+	
+	public boolean isSupported() {				
+		return true;
+	}
+}
+
+
