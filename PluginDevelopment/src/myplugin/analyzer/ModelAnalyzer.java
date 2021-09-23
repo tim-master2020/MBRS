67cf0cf79456cc4e21541c7e510995549529dd5e
OljaSekulic <olja.sekulic.77@gmail.com>
2021-09-23 18:34:48 +0200

    fixed for loop for enumerations

--- PluginDevelopment/src/myplugin/analyzer/ModelAnalyzer.java
+++ PluginDevelopment/src/myplugin/analyzer/ModelAnalyzer.java
@@ -223,7 +223,7 @@ public class ModelAnalyzer {
 	private FMEnumeration getEnumerationData(Enumeration enumeration, String packageName) throws AnalyzeException {
 		FMEnumeration fmEnum = new FMEnumeration(enumeration.getName(), packageName);
 		List<EnumerationLiteral> list = enumeration.getOwnedLiteral();
-		for (int i = 0; i < list.size() - 1; i++) {
+		for (int i = 0; i < list.size(); i++) {
 			EnumerationLiteral literal = list.get(i);
 			if (literal.getName() == null)  
 				throw new AnalyzeException("Items of the enumeration " + enumeration.getName() +
